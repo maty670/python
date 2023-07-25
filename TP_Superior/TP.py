bb6700ba2063116389938fdd0ef7a22085c9eeb1
MatiasEsposito <matiasesposito.10@hotmail.com>
2023-07-25 19:00:52 -0300

    "qwe"

--- TP_Superior/TP.py
+++ TP_Superior/TP.py
@@ -1,6 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sympy import *
+
+w = Symbol('w')
+t = Symbol('t', Real=True)
+
 if __name__ == "__main__":
     # Leer los puntos desde el archivo
-    with open("C:/Users/NELIA/Desktop/Matias/tp_superior/epiciclos.txt", "r") as file:
+    with open("D:/Datos/Desktop/python/TP_Superior/epiciclos.txt", "r") as file:
+        
         lines = file.readlines()[1:]    #Skipear la primer linea
 
 def parse(l):    
@@ -11,11 +19,15 @@ def parse(l):
 
 for x in lines:
     parse(x)
-    Radios = parse(x)[0]
-    Ck = parse(x)[1]
+    Radios=[]
+    Ck=[]
+    Radios.append(parse(x)[0])
+    Ck.append(parse(x)[1])
     print(f"{type(Radios)} {Radios}    {type(Ck)} {Ck}")
     
 
     
+
+#https://www.youtube.com/watch?v=0J31ACZ6jtk&ab_channel=SistemasInteligentes
 
  
