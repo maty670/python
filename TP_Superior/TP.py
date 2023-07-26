f78185d4cd9ebe7d98b46e781a38e27d98a982c7
maty670 <matiasesposito.10@hotmail.com>
2023-07-26 09:26:15 -0300

    "sadsd"

--- TP_Superior/TP.py
+++ TP_Superior/TP.py
@@ -1,29 +1,32 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sympy import *
+#import matplotlib.pyplot as plt
+#import numpy as np
+#from sympy import *
 
-w = Symbol('w')
-t = Symbol('t', Real=True)
+#w = Symbol('w')
+#t = Symbol('t', Real=True)
 
 if __name__ == "__main__":
     # Leer los puntos desde el archivo
-    with open("D:/Datos/Desktop/python/TP_Superior/epiciclos.txt", "r") as file:
-        
+    with open("C:/Users/NELIA/Desktop/Matias/python/TP_Superior/epiciclos.txt", "r") as file:
         lines = file.readlines()[1:]    #Skipear la primer linea
 
-def parse(l):    
-    n = l.split("    ")
-    n[0] = n[0].strip()
-    n[1] = n[1].strip().replace("i","").replace(" ","") + "j"
-    return [float(n[0]),complex(n[1])]
+def parseRad(f):    
+    f = f.split("    ")
+    radio = f[0].strip()
+    return float(radio)
+
+def parseCk(f):    
+    f = f.split("    ")
+    Ck = f[1].strip().replace("i","").replace(" ","") + "j"
+    return complex(Ck)
+    
+
 
 for x in lines:
-    parse(x)
-    Radios=[]
-    Ck=[]
-    Radios.append(parse(x)[0])
-    Ck.append(parse(x)[1])
-    print(f"{type(Radios)} {Radios}    {type(Ck)} {Ck}")
+    Radio = parseRad(x)
+    Ck = parseCk(x)
+    print(f"{type(Radio)} {Radio}       {type(Ck)} {Ck}")
+
     
 
     
