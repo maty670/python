b4a8d2952def376a522677852b76d63273552c55
MatiasEsposito <matiasesposito.10@hotmail.com>
2023-08-27 19:17:48 -0300

    "."

--- Curso-Python/04_Excepciones_ExprReg/01_excepciones.py
+++ Curso-Python/04_Excepciones_ExprReg/01_excepciones.py
@@ -1,11 +1,26 @@
-def sumar_dos():
+def dividir_dos():
     
     while True:
         a = input("Numero 1: ")
         b = input("Numero 2: ")
-        try:
-            return int(a) + int(b)
-        except:
-            print("Datos invalidos. No se pudo ejecutar la suma")
         
-print(sumar_dos())
+        #Intentando convertir los datos en numeros enteros
+        try:
+            resultado = int(a) / int(b)
+        #Si lanzo una excepcion, mostrar mensaje de error
+        except ValueError as e:
+            print(f"ERROR: Los datos no son enteros \n{type(e).__name__}")
+        except ZeroDivisionError as e:
+            print(f"ERROR: Entero dividido por 0 \n{type(e).__name__}")
+        #Si no lanzo una excepcion, salir del bucle
+        else:
+            break
+        
+    return resultado
+
+
+
+print(dividir_dos())
+
+
+# 07:23:40
