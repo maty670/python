7c5f0ce4ed430952783a9d3004c13664352662b5
MatiasEsposito <matiasesposito.10@hotmail.com>
2023-09-29 22:28:41 -0300

    "."

--- TP_Superior/TP2/Ej1/b/biseccion+newton.py
+++ TP_Superior/TP2/Ej1/b/biseccion+newton.py
@@ -1,34 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from mpmath import mp
+
+# Configura la precisión a 30 dígitos decimales
+mp.dps = 100
+
+
 # Funciones polinomicas en forma de tuplas
-P1 = (-0.00648,-0.2674,-2.662,-6.959)
-P2 = (-82.08,398.9,-651.7,371.8)
-P3 = (-0.342,3.791,-16.47,26.26)
-P4 = (0.4102,2.549,6.472,2.406)
-P5 = (-0.0166,-0.5157,-3.794,-1.834)
-P6 = (-0.007784,0.03998,-0.3053,-9.409)
-P7 = (-0.003877,0.1498,-0.3954,-0.7499)
-P8 = (0.0002778,-0.01556,0.7777,4.172)
-P9 = (0.1023,-0.3259,1.8,12.55)
+P1 = ('-0.00648','-0.2674','-2.662','-6.959')
+P2 = ('-82.08','398.9','-651.7','371.8')
+P3 = ('-0.342','3.791','-16.47','26.26')
+P4 = ('0.4102','2.549','6.472','2.406')
+P5 = ('-0.0166','-0.5157','-3.794','-1.834')
+P6 = ('-0.007784','0.03998','-0.3053','-9.409')
+P7 = ('-0.003877','0.1498','-0.3954','-0.7499')
+P8 = ('0.0002778','-0.01556','0.7777','4.172')
+P9 = ('0.1023','-0.3259','1.8','12.55')
 
 # Evaluar una funcion polinomica Px en un valor x
 def f(Px,x):
-    return Px[0]*x**3 + Px[1]*x**2 + Px[2]*x + Px[3]
+    Px0 = mp.mpf(float(Px[0]))
+    Px1 = mp.mpf(float(Px[1]))
+    Px2 = mp.mpf(float(Px[2]))
+    Px3 = mp.mpf(float(Px[3]))
+    return Px0*x**3 + Px1*x**2 + Px2*x + Px3
 
 
 def interseccion(Pa,Pb,x):
-    C1 = Pa[0] - Pb[0]
-    C2 = Pa[1] - Pb[1]
-    C3 = Pa[2] - Pb[2]
-    C4 = Pa[3] - Pb[3]
+    C1 = mp.mpf(Pa[0]) - mp.mpf(Pb[0])
+    C2 = mp.mpf(Pa[1]) - mp.mpf(Pb[1])
+    C3 = mp.mpf(Pa[2]) - mp.mpf(Pb[2])
+    C4 = mp.mpf(Pa[3]) - mp.mpf(Pb[3])
     return C1*x**3 + C2*x**2 + C3*x +C4
 
 def derivada(Pa,Pb,x):
-    C1 = Pa[0] - Pb[0]
-    C2 = Pa[1] - Pb[1]
-    C3 = Pa[2] - Pb[2]
-    C4 = Pa[3] - Pb[3]
+    C1 = mp.mpf(Pa[0]) - mp.mpf(Pb[0])
+    C2 = mp.mpf(Pa[1]) - mp.mpf(Pb[1])
+    C3 = mp.mpf(Pa[2]) - mp.mpf(Pb[2])
+    C4 = mp.mpf(Pa[3]) - mp.mpf(Pb[3])
     return 3*C1*x**2 + 2*C2*x + C3
 
 def newton_raphson(x0, tol, max_iter,Pa,Pb):
@@ -38,7 +48,7 @@ def newton_raphson(x0, tol, max_iter,Pa,Pb):
     while abs(interseccion(Pa,Pb,x)) > tol and it < max_iter:
         x = x - interseccion(Pa,Pb,x) / derivada(Pa,Pb,x)
         it += 1
-
+    
     return x,abs(interseccion(Pa,Pb,x)) ,it
 
 def biseccion(a,b,max_it,tol,Pa,Pb):
@@ -53,7 +63,7 @@ def biseccion(a,b,max_it,tol,Pa,Pb):
         it+=1
         
     #newton_raphson(x0,tolerancia,max_iterac,Pa,Pb)
-    raiz,residuo,iteracionesNewton = newton_raphson(m,1e-10,50,Pa,Pb)  
+    raiz,residuo,iteracionesNewton = newton_raphson(m,1e-30,50,Pa,Pb)  
     
     # Evaluar la raiz en el polinomio para obtener la imagen           
     fx = f(Pa,raiz)
@@ -65,7 +75,7 @@ def biseccion(a,b,max_it,tol,Pa,Pb):
 
 #biseccion(extremo_A,extremo_b,max_iteraciones,tolerancia,Polinomio_a,Polinomio_b)
 
-x1,y1,residuo_1,iteracionesNewton_1,iteracionesBiseccion_1 = biseccion(-18,-14,50,0.01,P1,P5)
+x1,y1,residuo_1,iteracionesNewton_1,iteracionesBiseccion_1 = biseccion(-16,-14,50,0.01,P1,P5)
 x2,y2,residuo_2,iteracionesNewton_2,iteracionesBiseccion_2 = biseccion(-5,-4,50,0.01,P1,P8)
 x3,y3,residuo_3,iteracionesNewton_3,iteracionesBiseccion_3 = biseccion(-4,-3,50,0.01,P1,P9)
 x4,y4,residuo_4,iteracionesNewton_4,iteracionesBiseccion_4 = biseccion(-2,-1,50,0.01,P1,P4)
@@ -111,10 +121,10 @@ def menorResiduo(residuos):
     for i in range(0,len(residuos)):
         res = residuos[i]
         punto = i+1
-        if abs(res) < abs(min) and res != 0:
+        if abs(res) < abs(min):
             min = res
             punto_min = punto
-        elif abs(res) > abs(max) and res != 0:
+        elif abs(res) > abs(max):
             max = res    
             punto_max = punto
     return min,max,punto_min,punto_max
@@ -124,10 +134,9 @@ lista_residuos = (residuo_1,residuo_2,residuo_3,residuo_4,residuo_5,residuo_6,re
 
 min,max,punto_min,punto_max = menorResiduo(lista_residuos)
 
-print(lista_residuos)
-
-print(f"""Distancia mas extensa: {max}, en el punto P{punto_min}
-Distancia max corta: {min}, en el punto P{punto_max}""")
+print(f"""Distancia max corta: {min}, en el punto P{punto_min}
+Distancia mas extensa: {max}, en el punto P{punto_max}
+""")
 
 
 
@@ -159,47 +168,47 @@ plt.scatter(x16, y16, color='y', marker='o', label='P16')
 
 
 # Graficar Polinomio P1 primero entre el punto 1 y el punto 4, y luego entre punto 6 y el punto 7
-P1_intervalo1 = np.linspace(x1, x4, 100)
+P1_intervalo1 = np.linspace(float(x1), float(x4), 100)
 plt.plot(P1_intervalo1, f(P1,P1_intervalo1), color='g')
 
 
 # Graficar Polinomio P2 primero entre el punto 14 y el punto 13, y luego entre punto 9 y el punto 7
-P2_intervalo1 = np.linspace(x14, x13, 100)
+P2_intervalo1 = np.linspace(float(x14), float(x13), 100)
 plt.plot(P2_intervalo1, f(P2,P2_intervalo1), color='r')
 
 
 # Graficar Polinomio P3 entre el punto 11 y el punto 10
-P3_intervalo1 = np.linspace(x11, x10, 100)
+P3_intervalo1 = np.linspace(float(x11), float(x10), 100)
 plt.plot(P3_intervalo1, f(P3,P3_intervalo1), color='b')
 
 
 # Graficar Polinomio P4 entre el punto 4 y el punto 5
-P4_intervalo1 = np.linspace(x4, x5, 100)
+P4_intervalo1 = np.linspace(float(x4), float(x5), 100)
 plt.plot(P4_intervalo1, f(P4,P4_intervalo1), color='c')
 
 
 # Graficar Polinomio P5 primero entre el punto 1 y el punto 15
-P5_intervalo1 = np.linspace(x1, x15, 100)
+P5_intervalo1 = np.linspace(float(x1), float(x15), 100)
 plt.plot(P5_intervalo1, f(P5,P5_intervalo1), color='m')
 
 
 # Graficar Polinomio P6 primero entre el punto 5 y el punto 10
-P6_intervalo1 = np.linspace(x5, x10, 100)
+P6_intervalo1 = np.linspace(float(x5), float(x10), 100)
 plt.plot(P6_intervalo1, f(P6,P6_intervalo1), color='k')
 
 
 # Graficar Polinomio P7 primero entre el punto 11 y el punto 12
-P7_intervalo1 = np.linspace(x11, x12, 100)
+P7_intervalo1 = np.linspace(float(x11), float(x12), 100)
 plt.plot(P7_intervalo1, f(P7,P7_intervalo1), color='g')
 
 
 # Graficar Polinomio P8 primero entre el punto 13 y el punto 12
-P8_intervalo1 = np.linspace(x13, x12, 100)
+P8_intervalo1 = np.linspace(float(x13), float(x12), 100)
 plt.plot(P8_intervalo1, f(P8,P8_intervalo1), color='y')
 
 
 # Graficar Polinomio P9 primero entre el punto 14 y el punto 15
-P9_intervalo1 = np.linspace(x14, x15, 100)
+P9_intervalo1 = np.linspace(float(x14), float(x15), 100)
 plt.plot(P9_intervalo1, f(P9,P9_intervalo1), color='c')
 
 
